4f0f1e382cc68550d6a013c1614b7e4ca8b2699b
Getm26 <metiennedev@gmail.com>
2023-11-14 00:09:46 +0100

    add interface RoleRepository

---
+++ src/main/java/com/mercadona/BackendPromotionProduits/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.mercadona.BackendPromotionProduits.repository;
+
+import com.mercadona.BackendPromotionProduits.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
