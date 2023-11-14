64c4a89f0f12984f6492c2a959df812a1209bc44
Getm26 <metiennedev@gmail.com>
2023-11-14 13:32:17 +0100

    add notation CrossOrigin modification @RequestMapping(/mercadona)

--- src/main/java/com/mercadona/BackendPromotionProduits/controller/ProduitController.java
+++ src/main/java/com/mercadona/BackendPromotionProduits/controller/ProduitController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/produit")
+@CrossOrigin(origins = "http://localhost:8092")
+@RequestMapping("/mercadona")
 @AllArgsConstructor
 public class ProduitController {
 
