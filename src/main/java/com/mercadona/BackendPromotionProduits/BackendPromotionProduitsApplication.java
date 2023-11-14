7e0ef92a5d8288f6467831f5feedc9b6874b6e3a
Getm26 <metiennedev@gmail.com>
2023-11-14 15:32:33 +0100

    mise en form

--- src/main/java/com/mercadona/BackendPromotionProduits/BackendPromotionProduitsApplication.java
+++ src/main/java/com/mercadona/BackendPromotionProduits/BackendPromotionProduitsApplication.java
@@ -2,8 +2,6 @@ package com.mercadona.BackendPromotionProduits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.web.authentication.AuthenticationFilter;
 
 @SpringBootApplication
 public class BackendPromotionProduitsApplication {
